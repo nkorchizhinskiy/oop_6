fd660708805be964270134044f98b146dc511363
Nazar Korchizhinskiy <korchizhinskiy@gmail.com>
2022-04-30 16:14:11 +0700

    Code refactoring

--- logic.py
+++ logic.py
@@ -10,11 +10,11 @@ from PyQt5.QtGui import QPainter, \
                         QBrush, \
                         QColor
 from PyQt5.QtCore import QPointF
-from matplotlib.pyplot import draw
 
 
 
 class MainWindow(QDialog):
+
     def __init__(self):
         super().__init__()
         
@@ -23,10 +23,10 @@ class MainWindow(QDialog):
 
         #// Create Widgets
         self.spinbox = QSpinBox(self)
+        self.spinbox.setPrefix('Скорость: ')
         self.spinbox.setMinimum(1)
         self.spinbox.setMaximum(100)
         self.spinbox.move(50, 50)
-        self.spinbox.setPrefix('Скорость: ')
 
         self.button_1 = QPushButton('Начать', self)
         self.button_2 = QPushButton('Выход', self)
@@ -36,14 +36,16 @@ class MainWindow(QDialog):
         #// SIGNALS
         self.button_1.clicked.connect(lambda: self.board.start_game(True))
         self.spinbox.valueChanged.connect(lambda: self.board.change_speed(
-            self.spinbox.value()))
+                                                  self.spinbox.value()))
         self.button_2.clicked.connect(lambda: sys.exit())
 
-        #// Create Layout
+        #// Create Layout --->
+        #// Horizontal layout
         buttons_layout = QHBoxLayout()
         buttons_layout.addWidget(self.button_1)
         buttons_layout.addWidget(self.button_2)
         
+        #// Vertical Layout
         layout = QVBoxLayout()
         layout.addWidget(self.spinbox)
         layout.addWidget(self.board)
@@ -55,13 +57,16 @@ class Board(QDialog):
     
     def __init__(self, spinbox):
         super().__init__()
-        self.setStyleSheet('background-color: #CCF')
+        self.spinbox = spinbox
+
+        #// Start ball's position
         self.x = 150
         self.y = -1
+
+        self.setStyleSheet('background-color: #CCF')
         self.flag_x = 1
         self.flag_y = 1
         self.is_game_run = False
-        self.spinbox = spinbox
     
     def paintEvent(self, event):
         self.painter = QPainter()
@@ -70,9 +75,11 @@ class Board(QDialog):
         self.painter.end()
 
     def draw_rect(self):
+        #// Set brush to fill ellipse
         brush = QBrush(QColor(255, 0, 0))
         self.painter.setBrush(brush)
         self.painter.drawEllipse((QPointF(self.x, self.y)), 40, 40)
+
         #// Change x Axis. Set Flag to change.
         if self.x <= 40:
             self.flag_x = 1
@@ -100,9 +107,11 @@ class Board(QDialog):
             self.update()
 
     def change_speed(self, value):
+        """Change speed of ellipse in conformity with spinbox value"""
         self.spinbox = value
 
     def start_game(self, start_flag):
+        """Set True value, which help to start ball's moving"""
         if start_flag:
             self.is_game_run = True
             self.update()
